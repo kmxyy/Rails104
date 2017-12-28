48c5f45e029d3689c4bdb9445d1467ac2f83b636
1424875746@qq.com <32759217+kmxyy@users.noreply.github.com>
2017-12-28 17:01:00 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
   @groups=Group.all
@@ -19,7 +19,7 @@ class GroupsController < ApplicationController
     @group.user=current_user
 
     if @group.save
-
+       current_user.join!(@group)
     redirect_to groups_path
   else
     render :new
@@ -48,6 +48,30 @@ class GroupsController < ApplicationController
     redirect_to groups_path
     flash[:alert]="Delete Success!"
   end
+
+  def join
+    @group=Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice]="加入本讨论组成功!"
+    else
+      flash[:warning]="你已经是本讨论组成员了！"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group=Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert]="已退出本讨论组！"
+    else
+      flash[:warning]="你不是本讨论组成员，怎么退出啊？"
+    end
+    redirect_to group_path(@group)
+  end
+
   private
 
   def find_group_and_check_permission
@@ -56,8 +80,7 @@ class GroupsController < ApplicationController
       redirect_to root_path,alert:"You have no peermission."
     end
   end
-
-  def group_params
+ def group_params
     params.require(:group).permit(:title, :description)
   end
 
