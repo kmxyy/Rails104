89e4018590610e258f22d0e429f36e056051a7ef
1424875746@qq.com <32759217+kmxyy@users.noreply.github.com>
2018-01-04 11:05:06 +0800

    posts/edit/delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,24 @@ class PostsController < ApplicationController
     @post=Post.new
   end
 
+  def edit
+    @group=Group.find(params[:group_id])
+    @post=Post.find(params[:id])
+    @post.group=@group
+
+  end
+
+  def update
+    @group=Group.find(params[:group_id])
+    @post=Post.find(params[:id])
+    @post.group=@group
+    @post.user=current_user
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice:"编缉成功！"
+    else
+      render :edit
+    end
+  end
 
   def create
   @group=Group.find(params[:group_id])
@@ -19,6 +37,15 @@ class PostsController < ApplicationController
     render :new
   end
  end
+
+  def destroy
+    @group=Group.find(params[:group_id])
+    @post=Post.find(params[:id])
+    @post.group=@group
+    @post.destroy
+    flash[:alert]="删除成功！"
+    redirect_to account_posts_path
+  end
 
 private
 
